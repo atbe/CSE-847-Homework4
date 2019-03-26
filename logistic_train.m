302ff9d807f738bd7de7d7142bee64cfef9fd5a7
@be <abeahmed2@gmail.com>
2019-03-26 18:46:13 -0400

    Added solved problem 1 with figure file.

--- logistic_train.m
+++ logistic_train.m
@@ -4,22 +4,22 @@ function [weights] = logistic_train(data, labels, epsilon, maxiter)
 % code to train a logistic regression classifier
 %
 % INPUTS:
-% data = n * (d+1) matrix with n samples and d features, where
-%       column d+1 is all ones (corresponding to the intercept term)
+% data = n * (d+1) matrix withn samples and d features, where
+% column d+1 is all ones (corresponding to the intercept term)
 % labels = n * 1 vector of class labels (taking values 0 or 1)
 % epsilon = optional argument specifying the convergence
-%       criterion - if the change in the absolute difference in
-%       predictions, from one iteration to the next, averaged across
-%       input features, is less than epsilon, then halt
-%       (if unspecified, use a default value of 1e-5)
+% criterion - if the change in the absolute difference in
+% predictions, from one iteration to the next, averaged across
+% input features, is less than epsilon, then halt
+% (if unspecified, use a default value of 1e-5)
 % maxiter = optional argument that specifies the maximum number of
-%       iterations to execute (useful when debugging in case your
-%       code is not converging correctly!)
-%       (if unspecified can be set to 1000)
+% iterations to execute (useful when debugging in case your
+% code is not converging correctly!)
+% (if unspecified can be set to 1000)
 %
 % OUTPUT:
 % weights = (d+1) * 1 vector of weights where the weights correspond to
-%       the columns of "data"
+% the columns of "data"
 %
 
 if nargin == 2
@@ -29,4 +29,25 @@ if nargin < 4
     maxiter = 1000;
 end
 weights = zeros(size(data,2),1);
+
+iteration = 0;
+
+while (iteration <= maxiter)
+    y = sigmf(data * weights, [1 0]);
+    R = diag (y .* (1 - y) );
+    
+    % Adding scalar to avoid matrix being close to signular
+    noise = 0.01;
+    R = R + noise * eye(length(R));
+    z = (data * weights) - (R^(-1) * (y - labels));
+    weights = (data' * R * data)^(-1) * data' * R * z;    
+    y_new = sigmf(data * weights,[1 0]);
+    predicition_magnitude_difference = mean(abs(y_new - y));   
+    
+    if (predicition_magnitude_difference < epsilon)
+        break;
+    end
+    iteration = iteration + 1;
+end
+
 end
--- problem_1.m
+++ problem_1.m
@@ -7,14 +7,13 @@ test_accuracies = [0, 0, 0, 0, 0, 0]';
 train_sizes = [200, 500, 800, 1000, 1500, 2000]';
 w = zeros(6, size(X,2));
 
-
 for i = 1:6
     n = train_sizes(i);
     X_train = X(1:n, :);
     y_train = y(1:n);
-    log_reg = logistic_train(X_train, y_train);
+    log_reg = logistic_train(X_train, y_train, 1e-5, 1000);
     
-    y_pred = sigmf(X_test * log_reg,[1 0]);
+    y_pred = round(sigmf(X_test * log_reg,[1 0]));
     test_accuracies(i) = sum(y_test== y_pred) / numel(y_test);
 end
 
@@ -23,3 +22,4 @@ plot(train_sizes, test_accuracies, 'o-');
 title('Problem 1: Logistic Regression Experiment');
 xlabel('Number of elements (n)');
 ylabel('Testing Accuracy');
+saveas(gcf, 'problem-1-accuracy.png');
